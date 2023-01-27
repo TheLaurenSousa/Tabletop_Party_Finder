8a1a9b4c6ed523ecfb5e4c50ad596476eed78907
Lauren Sousa <thelaurensousa@gmail.com>
2023-01-27 13:59:02 -0500

    basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './views/LandingPage';
+import Login from './views/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route element={<LandingPage/>} path='/'/>
+        <Route element={<Login/>} path='login'/>
       </Routes>
     </div>
   );
--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -5,6 +5,9 @@ export default () => {
     return (
         <div>
             <h1>Welcome to the Tabletop Party Finder!</h1>
+            <Link to={'/login'}>
+                <h2>Let's Dive In</h2>
+            </Link>
         </div>
     )
 }
