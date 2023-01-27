2a183f11abbaab7032119cce552807770e688b4a
Lauren Sousa <thelaurensousa@gmail.com>
2023-01-27 13:53:16 -0500

    Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './views/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tabletop Party Finder</h1>
+      <Routes>
+        <Route element={<LandingPage/>} path='/'/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/views/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            <h1>Welcome to the Tabletop Party Finder!</h1>
+        </div>
+    )
+}
