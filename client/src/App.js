ed6fb16b4a3280afe35449414bce4bd0dca5938b
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-02 12:39:54 -0500

    Created Basic Routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './views/LandingPage';
 import Login from './views/Login';
 import Home from './views/Home';
+import SearchGames from './views/SearchGames';
+import SearchGMs from './views/SearchGMs';
+import ViewGame from './views/ViewGame';
+import ViewGM from './views/ViewGM';
+import Profile from './views/Profile';
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
         <Route element={<LandingPage/>} path='/'/>
         <Route element={<Login/>} path='login'/>
         <Route element={<Home/>} path='home'/>
+        <Route element={<SearchGames/>} path='searchGames'/>
+        <Route element={<SearchGMs/>} path='searchGMs'/>
+        <Route element={<ViewGame/>} path='game'/>
+        <Route element={<ViewGM/>} path='gm'/>
+        <Route element={<Profile/>} path='profile'/>
       </Routes>
     </div>
   );
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
 import Nav from '../components/nav';
 
 export default () => {
     const username = localStorage.getItem('username');
+    const navigate = useNavigate();
+
+    const onGameClick = e => {
+        navigate('/searchGames');
+    }
+
+    const onGMClick = e => {
+        navigate('/searchGMs');
+    }
 
     return (
         <div>
             <Nav/>
             <h1>Welome {username}!</h1>
+            <button onClick={onGameClick}>Find Open Games</button>
+            <button onClick={onGMClick}>Find Game Masters</button>
+            <p>Placeholder for user's active games</p>
+            <p>Placeholder for user's game history</p>
         </div>
     )
 }
