1b993ad8841c64e1494863b7fcce5db317840e14
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-02 12:47:35 -0500

    Routes build out

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -6,15 +6,6 @@ import logoutProcess from './helpers/logoutHelper';
 const Nav = () => {
     const navigate = useNavigate();
 
-    const onLogoutClick = e => {
-        logoutProcess()
-            .then(res => {
-                localStorage.clear();
-            })
-            .catch(err => console.log(err));
-            navigate('/');
-    }
-
     const onHomeClick = e => {
         navigate('/home');
     }
@@ -27,12 +18,26 @@ const Nav = () => {
         navigate('/searchGMs');
     }
 
+    const onProfileClick = e => {
+        navigate('/profile');
+    }
+
+    const onLogoutClick = e => {
+        logoutProcess()
+            .then(res => {
+                localStorage.clear();
+            })
+            .catch(err => console.log(err));
+            navigate('/');
+    }
+
     return (
         <div className='nav'>
             <h2 onClick={onHomeClick}>Tabletop Party Finder</h2>
             <ul>
                 <li onClick={onGameClick}>Find Games</li>
                 <li onClick={onGMClick}>Find GM</li>
+                <li onClick={onProfileClick}>Profile</li>
                 <li onClick={onLogoutClick}>Logout</li>
             </ul>
         </div>
---
+++ client/src/views/SearchGames.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import Nav from '../components/nav';
+
+export default () => {
+
+    return (
+        <div>
+            <Nav/>
+            <h1>Placeholder for Search Games Page</h1>
+        </div>
+    )
+}
